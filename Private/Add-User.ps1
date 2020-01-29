24cf7250f5bdc4b6c25bc7e0afae6c1b797251c0
it-joe <joku@it-joe.de>
2020-01-29 14:46:01 +0100

    FEAT(add-user): Add user creation functionality
     * Add function to create user accounts in AAD
     * Add function to create random first and last name combinations

---
+++ Private/Add-User.ps1
@@ -0,0 +1,68 @@
+function Add-User {
+    <#
+    .SYNOPSIS
+        Creates user accounts in Azure Active Directory.
+    .DESCRIPTION
+        Creates user accounts in Azure Active Directory.
+    .PARAMETER UserCount
+        The number of user accounts to create.
+    .PARAMETER OrgName
+        The name of the organization.
+    .PARAMETER UsageLocation
+        ISO 3166-1 alpha-2 country code.
+        Before a license can be assigned to a user, the administrator has to specify the usage location property on the user. 
+        Some Microsoft services are not available in all locations.
+    .EXAMPLE
+        PS C:\> Add-User -UserCount 5 -OrgName Contoso -UsageLocation DE
+        Creates 5 users in DE for the Contoso organization.
+    .INPUTS
+        None. You cannot pipe objects to Add-User.
+    .OUTPUTS
+        None. Add-User does not generate any output.
+    .NOTES
+        The use of a common password here is for automation and ease of configuration for a test environment.
+        Obviously, this is highly discouraged for production subscriptions.
+    #>
+    [CmdletBinding()]
+    param (
+        # The number of users to create
+        [Parameter(Mandatory)]
+        [int]
+        $UserCount,
+        
+        # The name of the organization 
+        [Parameter(Mandatory)]
+        [string]
+        $OrgName,
+
+        # Usage location
+        [Parameter(Mandatory)]
+        [string]
+        $UsageLocation
+    )
+
+    # Common user account password
+    $CommonPwd = "AweS0me@PW" 
+    $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $PasswordProfile.Password = $CommonPwd
+    
+    # Create object containing random first and last name combinations
+    $ObjNames = Invoke-RandomUserName -UserCount $UserCount
+
+    # Create users
+    for ($i = 0; $i -lt $UserCount; $i++) {
+        $UserUPN = "$($ObjNames[$i].FirstName)$($ObjNames[$i].SurName)@" + $OrgName + ".onmicrosoft.com"
+
+        $NewAzureADUserSplat = @{
+            DisplayName       = "$($ObjNames[$i].FirstName) $($ObjNames[$i].SurName)"
+            GivenName         = $ObjNames[$i].FirstName
+            SurName           = $ObjNames[$i].SurName
+            UserPrincipalName = $UserUPN
+            UsageLocation     = $UsageLocation
+            AccountEnabled    = $true
+            PasswordProfile   = $PasswordProfile
+            MailNickName      = "$($ObjNames[$i].FirstName)$($ObjNames[$i].SurName)"
+        }
+        New-AzureADUser @NewAzureADUserSplat
+    }
+}
---
+++ Private/Invoke-RandomUserName.ps1
@@ -0,0 +1,51 @@
+﻿function Invoke-RandomUserName {
+    <#
+    .SYNOPSIS
+        Creates and returns an object containing random first and last name combinations.
+    .DESCRIPTION
+        Creates and returns an object containing random first and last name combinations.
+        The object consists of the FirstName and SurName property, which contain the specified number 
+        of random name values.
+    .PARAMETER UserCount
+        The number of random data sets to create.
+    .EXAMPLE
+        PS C:\> Invoke-RandomUserName -UserCount 25
+        Returns an object containing random values for the FirstName and SurName properties.
+    .INPUTS
+        None. You cannot pipe objects to Invoke-RandomUserName.
+    .OUTPUTS
+        None. Invoke-RandomUserName does not generate any output.
+    #>
+    [CmdletBinding()]
+    param (
+        # The number of random data sets to create
+        [Parameter(Mandatory)]
+        [int]
+        $UserCount        
+    )
+    
+    $ObjNames = @()
+    
+    # Start generating...
+    1..$UserCount | ForEach-Object {
+
+        $Male = Get-Random 2 # Flip a coin for gender
+        
+        # Pick a first name based on the gender
+        if ($Male) {
+            $FirstName = Get-Content "$PSScriptRoot\Males.txt" | Get-Random 
+        }
+        else { 
+            $FirstName = Get-Content "$PSScriptRoot\Females.txt" | Get-Random 
+        }
+    
+        $SurName = Get-Content "$PSScriptRoot\Surnames.txt" | Get-Random # Pick a surname
+
+        # Create object to save names
+        $ObjNames += [PSCustomObject]@{
+            FirstName = $FirstName
+            SurName   = $SurName
+        }
+    }
+    return $ObjNames
+}
